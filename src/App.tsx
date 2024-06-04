17cc7ffb7e29d603ff82093663aa2df59b3f9363
Dan <danporeda@yahoo.com>
2024-06-04 14:12:46 -0400

    created type Place in src/api, imported to App and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import type { Place } from './api/Place';
 import Map from './components/Map';
 import LocationSearch from './components/LocationSearch';
 
 function App() {
+  
+
   return (
    <div className="h-screen w-screen grid grid-cols-12">
     <div className="col-span-3 p-3">
