406c7c425e479b91a1ff9f2b38865169c56d12b7
Dan <danporeda@yahoo.com>
2024-06-04 14:19:59 -0400

    created MapProps in Map component. added place state to App component, passed place prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import type { Place } from './api/Place';
+import { useState } from 'react';
 import Map from './components/Map';
 import LocationSearch from './components/LocationSearch';
 
 function App() {
-  
+  const [place, setPlace] = useState<Place | null>(null);
 
   return (
    <div className="h-screen w-screen grid grid-cols-12">
@@ -11,7 +12,7 @@ function App() {
       <LocationSearch />
     </div>
     <div className="col-span-9">
-      <Map />
+      <Map place={place}/>
     </div>
    </div>
   )
