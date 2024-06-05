96ad4fa4bbb9e306cc924f5876cddbef93c4bf02
Dan <danporeda@yahoo.com>
2024-06-04 21:30:21 -0400

    implemented search fucntion into LocationSearch: in the handleSubmit function, fetches locations which are then set as places state. implemented return JSX to render places with button. TODO: create the onClick function which is the onPlaceCLick

--- src/api/search.ts
+++ src/api/search.ts
@@ -19,11 +19,12 @@ export const search = async (term: string) => {
   const data: SearchResponse = await res.json();
 
   const places: Place[] = data.features.map((feature) => {
+    // instructor says longitude is [0], but i suspect its the opposite
     return {
       id: feature.properties.place_id,
       name: feature.properties.display_name,
-      longitude: feature.geometry.coordinates[1],
-      latitude: feature.geometry.coordinates[0]
+      longitude: feature.geometry.coordinates[0],
+      latitude: feature.geometry.coordinates[1]
     }
   });
 
