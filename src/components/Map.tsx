379bec5ac3733e67a77be62d7757d8560b685155
Dan <danporeda@yahoo.com>
2024-06-05 13:41:11 -0400

    installed leaflet, react-leaflet dependencies. imported modules into Map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,8 @@
+import 'leaflet/dist/leaflet.css';
 import type { Place } from '../api/Place';
+import type { Map as LeafletMap } from 'leaflet';
+import { useEffect, useRef } from 'react';
+import { MapContainer, Tilelayer, Marker } from 'react-leaflet';
 
 interface MapProps {
   place: Place | null;
